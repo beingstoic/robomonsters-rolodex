509f3a2fda61269f549381e5863202425da1968d
ansiblerg <rishabhgoyal0001@gmail.com>
2020-07-16 15:22:43 +0530

    Updates

---
+++ precache-manifest.2d596f823e48defd585c293f29d9a87a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39db750a2858949c6faa6e4b6e85d360",
+    "url": "/robomonsters-rolodex/index.html"
+  },
+  {
+    "revision": "7934acbb4f6eefe0ba77",
+    "url": "/robomonsters-rolodex/static/css/main.efb2d4fb.chunk.css"
+  },
+  {
+    "revision": "8a1e101f4890b66c743d",
+    "url": "/robomonsters-rolodex/static/js/2.70d5715d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robomonsters-rolodex/static/js/2.70d5715d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7934acbb4f6eefe0ba77",
+    "url": "/robomonsters-rolodex/static/js/main.aa7f17c1.chunk.js"
+  },
+  {
+    "revision": "eede59f3628e1a284482",
+    "url": "/robomonsters-rolodex/static/js/runtime-main.15e5968f.js"
+  }
+]);
